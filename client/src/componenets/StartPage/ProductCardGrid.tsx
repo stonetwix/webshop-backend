3041ecd8c4a694559fa785d6c4ff1b273bf50452
Moa Stenqvist <moa@stonetwig.se>
2021-05-19 14:28:07 +0200

    Add category selection with tags to start page.

--- client/src/componenets/StartPage/ProductCardGrid.tsx
+++ client/src/componenets/StartPage/ProductCardGrid.tsx
@@ -1,14 +1,24 @@
 import { Component, ContextType, CSSProperties } from 'react';
-import { Card, Col, List, Row, message } from 'antd';
+import { Card, Col, List, Row, message, Select } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../contexts/CartContext';
 
 const { Meta } = Card;
+const { Option } = Select;
+
 const success = () => {
     message.success('The product was added to the cart', 5);
 };
 
+const children: any = [];
+for (let i = 10; i < 36; i++) {
+    children.push(
+        <Option value={i.toString(36) + i} key={i.toString(36) + i}>
+        {i.toString(36) + i}
+        </Option>
+    )
+}
 export interface Product {
     _id: string
     title: string
@@ -31,45 +41,66 @@ class ProductCardGrid extends Component<State> {
     async componentDidMount() {
         const products = await getProducts();
         this.setState({ products: products });
-      }
+    }
+
+    handleChange(value: any) {
+        console.log(`selected ${value}`);
+    }
         
     render() {
         const { addProductToCart } = this.context;
-        return(    
-            <Row style={cardContainer}>
-                <Col span={24} style={columnStyle}>
-                    <List
-                        grid={{
-                            gutter: 25,
-                            xs: 1,
-                            sm: 2,
-                            md: 2,
-                            lg: 4,
-                            xl: 4,
-                            xxl: 4,
-                        }}
-                        dataSource={this.state.products}
-                        renderItem={item => (
-                            <List.Item>
-                                <Link to={'/product/' + item._id}>
-                                    <Card
-                                        hoverable
-                                        cover={<img src={item.imageUrl} alt='product' />}
-                                        actions={[
-                                            <ShoppingCartOutlined 
-                                                style={{ fontSize: '2rem' }}
-                                                onClick={(e) => {success(); e.preventDefault(); addProductToCart(item, undefined)}} 
-                                            />
-                                        ]}
-                                    >
-                                    <Meta title={item.title} description={item.price + ' kr'} />
-                                    </Card>
-                                </Link>
-                            </List.Item>
-                        )}    
-                    />
-                </Col>
-            </Row>
+        return(
+            <>
+                <Row style={selectContainer}>
+                    <Col span={24}>
+                        <h3>Filter products</h3>
+                        <Select
+                            mode="multiple"
+                            allowClear
+                            style={{ width: '100%' }}
+                            placeholder="Please select"
+                            defaultValue={['all']}
+                            onChange={this.handleChange}
+                            >
+                            {children}
+                        </Select>
+                    </Col>
+                </Row> 
+                <Row style={cardContainer}>
+                    <Col span={24} style={columnStyle}>
+                        <List
+                            grid={{
+                                gutter: 25,
+                                xs: 1,
+                                sm: 2,
+                                md: 2,
+                                lg: 4,
+                                xl: 4,
+                                xxl: 4,
+                            }}
+                            dataSource={this.state.products}
+                            renderItem={item => (
+                                <List.Item>
+                                    <Link to={'/product/' + item._id}>
+                                        <Card
+                                            hoverable
+                                            cover={<img src={item.imageUrl} alt='product' />}
+                                            actions={[
+                                                <ShoppingCartOutlined 
+                                                    style={{ fontSize: '2rem' }}
+                                                    onClick={(e) => {success(); e.preventDefault(); addProductToCart(item, undefined)}} 
+                                                />
+                                            ]}
+                                        >
+                                        <Meta title={item.title} description={item.price + ' kr'} />
+                                        </Card>
+                                    </Link>
+                                </List.Item>
+                            )}    
+                        />
+                    </Col>
+                </Row>
+            </>
         )
     }
 }
@@ -85,11 +116,21 @@ const cardContainer: CSSProperties = {
     paddingBottom: '8rem',
 }
 
+const selectContainer: CSSProperties = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'space-around',
+    width: '80%',
+    margin: 'auto',
+    paddingBottom: '2rem',
+    paddingTop: '2rem',
+}
+
 const columnStyle: CSSProperties = {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: '3rem',
+    marginTop: '2rem',
 }
 
 const getProducts = async () => {
