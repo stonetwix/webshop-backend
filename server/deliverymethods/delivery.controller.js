49cd0f06b80e97f5794eceb4858055ac8a005b23
Amanda <amanda.e.samuelsson@hotmail.com>
2021-05-21 14:07:14 +0200

    Fixed import database

--- server/deliverymethods/delivery.controller.js
+++ server/deliverymethods/delivery.controller.js
@@ -15,3 +15,12 @@ exports.getOneDeliveryMethod = async (req, res) => {
     }
 }
 
+exports.addDelivery = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const newDelivery = await DeliveryModel.create(req.body);;
+    res.status(201).json(newDelivery);
+}
+
--- server/deliverymethods/delivery.router.js
+++ server/deliverymethods/delivery.router.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const deliveryRouter = express.Router();
 const controller = require('./delivery.controller');
-// const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 deliveryRouter
     .get('/api/delivery', controller.getAllDeliveryMethods)
     .get('/api/delivery/:id', controller.getOneDeliveryMethod)
+    .post('/api/delivery',
+        body('company').not().isEmpty(),
+        body('price').not().isEmpty(),
+        body('deliverytime').not().isEmpty(),
+        controller.addDelivery);
 
 module.exports = deliveryRouter;
