d4cb711254569fe16aeac72f7067642903474c15
Moa Stenqvist <moa@stonetwig.se>
2021-05-28 11:14:44 +0200

    Fix categories, delivery routers.

--- server/categories/categories.router.js
+++ server/categories/categories.router.js
@@ -2,18 +2,21 @@ const express = require('express');
 const categoriesRouter = express.Router();
 const controller = require('./categories.controller');
 const { body, validationResult } = require('express-validator');
+const auth = require('../auth');
 
 categoriesRouter
     .get('/api/categories', controller.getAllCategories)
     .get('/api/categories/:id', controller.getOneCategory)
     .post('/api/categories',
+        auth.secureWithAdmin,
         body('name').not().isEmpty(),
-        //body('products').not().isEmpty(),
         controller.addCategory)
     .put('/api/categories/:id',
+        auth.secureWithAdmin,
         body('name').not().isEmpty(),
-        //body('products').not().isEmpty(),
         controller.editCategory)
-    .delete('/api/categories/:id', controller.deleteCategory);
+    .delete('/api/categories/:id', 
+        auth.secureWithAdmin,    
+        controller.deleteCategory);
 
 module.exports = categoriesRouter;
