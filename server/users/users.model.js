f793e4f81ff6e539d903fa364ecaef5dc7499d22
msmalinosterberg <msmalinosterberg@gmail.com>
2021-05-31 15:07:08 +0200

    ..

--- server/users/users.model.js
+++ server/users/users.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true }, 
     password: {type: String, required: true, select: false },
-    role: { type: String, required: true }
+    role: { type: String, required: true },
+    adminRequest: {type: Boolean, required: true }
 }); 
 
 const UserModel = mongoose.model('User', userSchema); 
